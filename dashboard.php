bb4c1509639be91f0123d5866f55ca52b9108af9
yube01 <yubraja008@gmail.com>
2023-08-25 20:21:34 +0545

    quick design

--- dashboard.php
+++ dashboard.php
@@ -2,45 +2,176 @@
 
 <head>
     <title>EscapePlanner</title>
+    <link rel="stylesheet" href="./dashboard.css">
 </head>
 
 <?php
-    session_start();
-    include "dbConfig.php";
-    if (isset($_SESSION['user_id'])) {
+session_start();
+include "dbConfig.php";
+if (isset($_SESSION['user_id'])) {
 
-        echo "welcome" . $_SESSION['user_id'];
-    }
+    echo "welcome" . $_SESSION['user_id'];
+}
 
-    ?>
+?>
+
 
 <body>
-   <div class="header">
-    <div class="logo">
-        <img src="./assets/logo.png" alt="">
-    </div>
-    <button>
-        <a href="logout.php">Logout</a>
-    </button>
-    <div class="options">
-        <div class="destination"></div>
-        <div class="user"></div>
+    <div class="header">
+        <div class="logo">
+            <img src="./assets/logo.png" alt="" height="50px">
+            <p>EscapePlanner</p>
+        </div>
 
-    </div>
-   </div>
-   <div class="main">
-   <form method="POST">
-   <h1>Search Hotel</h1>
-    <input type="text" placeholder="Search hotel" name="hotel">
-    <input type="submit" name="submit">
-   </form>
-   </div>
-   <div class="footer">
-    copyright 2023
-   </div>
-    
+        <div class="options">
+            <div class="username">Yube</div>
+            <button>
+                <a href="logout.php">Logout</a>
+            </button>
 
-   
+        </div>
+    </div>
+    <div class="main">
+        <h1>Available Rooms</h1>
+        <div class="card">
+            <div class="hotelImg">
+                <img src="./assets/ColorWall-1pgxjg.jpg" alt="">
+            </div>
+            <div class="details">
+                <div class="hdetails">
+                    <p class="hname">Hotel Bagmati</p>
+                    <span class="loc">
+                    <p>Kathmandu</p>
+                    <p>3.5 km from center</p>
+                    </span>
+                </div>
+                <div class="desc">
+                    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nobis laudantium totam rem nisi labore
+                    animi dolorum illum tempora rerum delectus.
+                </div>
+                <div class="rooms">
+                    2 rooms left
+                </div>
+                <div class="price">
+                    <p>NPR 5,4500</p>
+                    <p>Includes taxes and fees</p>
+                </div>
+
+
+
+            </div>
+        </div>
+        <div class="card">
+            <div class="hotelImg">
+                <img src="./assets/logo.png" height="300px" alt="">
+            </div>
+            <div class="details">
+                <div class="hdetails">
+                    <p>Hotel Bagmati</p>
+                    <p>Kathmandu</p>
+                    <p>3.5 km from center</p>
+                </div>
+                <div class="desc">
+                    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nobis laudantium totam rem nisi labore
+                    animi dolorum illum tempora rerum delectus.
+                </div>
+                <div class="rooms">
+                    2 rooms left
+                </div>
+                <div class="price">
+                    <p>NPR 5,4500</p>
+                    <p>Includes taxes and fees</p>
+                </div>
+
+
+
+            </div>
+        </div>
+        <div class="card">
+            <div class="hotelImg">
+                <img src="./assets/ColorWall-1pgxjg.jpg" height="300px" alt="">
+            </div>
+            <div class="details">
+                <div class="hdetails">
+                    <p>Hotel Bagmati</p>
+                    <p>Kathmandu</p>
+                    <p>3.5 km from center</p>
+                </div>
+                <div class="desc">
+                    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nobis laudantium totam rem nisi labore
+                    animi dolorum illum tempora rerum delectus.
+                </div>
+                <div class="rooms">
+                    2 rooms left
+                </div>
+                <div class="price">
+                    <p>NPR 5,4500</p>
+                    <p>Includes taxes and fees</p>
+                </div>
+
+
+
+            </div>
+        </div>
+        <div class="card">
+            <div class="hotelImg">
+                <img src="./assets/ColorWall-1pgxjg.jpg" height="300px" alt="">
+            </div>
+            <div class="details">
+                <div class="hdetails">
+                    <p>Hotel Bagmati</p>
+                    <p>Kathmandu</p>
+                    <p>3.5 km from center</p>
+                </div>
+                <div class="desc">
+                    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nobis laudantium totam rem nisi labore
+                    animi dolorum illum tempora rerum delectus.
+                </div>
+                <div class="rooms">
+                    2 rooms left
+                </div>
+                <div class="price">
+                    <p>NPR 5,4500</p>
+                    <p>Includes taxes and fees</p>
+                </div>
+
+
+
+            </div>
+        </div>
+        <div class="card">
+            <div class="hotelImg">
+                <img src="./assets/ColorWall-1pgxjg.jpg" height="300px" alt="">
+            </div>
+            <div class="details">
+                <div class="hdetails">
+                    <p>Hotel Bagmati</p>
+                    <p>Kathmandu</p>
+                    <p>3.5 km from center</p>
+                </div>
+                <div class="desc">
+                    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nobis laudantium totam rem nisi labore
+                    animi dolorum illum tempora rerum delectus.
+                </div>
+                <div class="rooms">
+                    2 rooms left
+                </div>
+                <div class="price">
+                    <p>NPR 5,4500</p>
+                    <p>Includes taxes and fees</p>
+                </div>
+
+
+
+            </div>
+        </div>
+    </div>
+    <div class="footer">
+        copyright 2023
+    </div>
+
+
+
 </body>
 
 
