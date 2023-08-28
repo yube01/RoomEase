3999fc77d7867e6dece341908ab91d1a20d8b541
yube01 <yubraja008@gmail.com>
2023-08-28 17:01:07 +0545

    admin panel form

--- adminPanel.php
+++ adminPanel.php
@@ -1,8 +1,61 @@
 <html>
-    <head>
-        <title>Admin Panel</title>
-    </head>
-    <body>
-        Admin panel
-    </body>
+
+<head>
+    <title>Admin Panel</title>
+</head>
+
+<body>
+    <h1>Admin Dashboard</h1>
+    <form method="POST" enctype="multipart/form-data">
+        Location
+        <br>
+        <input type="text" placeholder="Location" name="loc">
+        <br>
+        description
+        <br>
+        <input type="text" placeholder="description" name="des">
+        <br>
+        Longitude and Latitude
+        <br>
+        <input type="text" placeholder="Actual Location" name="lng">
+        <br>
+        Images
+        <input type="file" name="Picture">
+        <br>
+        <input type="submit" name="submit">
+    </form>
+
+    <?php
+
+    include "dbConfig.php";
+    if(isset($_POST['submit'])){
+        $location = $_POST["loc"];
+        $desc =$_POST["des"];
+        $longlat = $_POST["lng"];
+        $Picture = $_FILES['Picture']['name'];
+        $temp = $_FILES['Picture']['tmp_name'];
+        $folder="picture/".$Picture;
+        move_uploaded_file($temp, $folder);
+
+       
+
+
+                $query  = "insert into room (Location,Descr,Longlat,Images) values('$location','$desc','$longlat','$folder')";
+                $sql = mysqli_query($conn,$query);
+
+                if($sql){
+                    
+                
+                    echo "data inserted";
+                }else{
+                    echo "not inserted";
+                }
+    }
+
+
+
+?>
+
+</body>
+
 </html>
---
+++ adminRegister.php
@@ -0,0 +1,62 @@
+<html>
+    <head>
+        <title>Admin Register page</title>
+    </head>
+    <body>
+        <div class="container">
+            <p>Register</p>
+            <form method="POST">
+                Username:
+                <br>
+                <input type="text" placeholder="Enter Username" name="username" required>
+                <br>
+                Email
+                <br>
+                <input type="email" placeholder="Enter email" name="email" required>
+                <br>
+                Password
+                <br>
+                <input type="password" name="password" required>
+                <br>
+                <input type="submit" value="Submit" name="submit"> 
+            </form>
+            <a href="login.php">Login page</a>
+            <?php
+            include "dbConfig.php";
+            if(isset($_POST['submit'])){
+                $user = $_POST["username"];
+                $password= md5($_POST ["password"]);
+                $email=$_POST ['email'];
+
+                //checking if email already exist
+                $checkEmail = "select * from auth where email='$email'";
+                $sqls = mysqli_query($conn,$checkEmail);
+                $num = mysqli_fetch_assoc($sqls);
+                if($num>0){
+                    echo "Email is already used";
+                    exit();
+                }
+
+
+
+                $query  = "insert into auth (username,email,password,isAdmin) values('$user','$email','$password','1')";
+                $sql = mysqli_query($conn,$query);
+
+                if($sql){
+                    
+                    header("Location: login.php");
+                    exit();
+                }else{
+                    echo "not inserted";
+                }
+
+                
+
+
+
+            }
+            ?>
+        </div>
+    </body>
+   
+</html>
